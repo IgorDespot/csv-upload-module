0e31ed28b47289b771d68a18a50a8727f09bb983
Drobniac <drobniac@gmail.com>
2018-02-19 15:30:51 +0100

    Add parser-promise-test.js to tests

---
+++ test/csv-parser/parser-promise-test.js
@@ -0,0 +1,8 @@
+let parsePromise = require('lib/csv-parser').parsePromise;
+
+describe('csv-parser.parsePromise', function () {
+
+    it('puts the lotion on its skin', function () {
+        expect().nothing();
+    });
+});
