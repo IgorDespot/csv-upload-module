7d5fb3ec19f8f52758118be62e72bcaebf7be6ce
Drobniac <drobniac@gmail.com>
2018-02-19 15:22:29 +0100

    Add more specs to parser-callback-test

--- test/csv-parser/parser-callback-test.js
+++ test/csv-parser/parser-callback-test.js
@@ -9,4 +9,21 @@ describe('csv-parser.parse', function () {
     it('should be a function', function () {
         expect(typeof parse).toMatch('function');
     });
+
+    it(`
+        should pass JSON converted valid string csv
+        to the provided callback function
+    `, function (done) {
+        parse('"first","second"\n1,2', function (err, out) {
+            expect(out).toEqual([{first:'1',second:'2'}]);
+            done();
+        })
+    });
+
+    it('should fail for an invalid csv file', function (done) {
+        parse('"first","second"\n12', function (err, out) {
+            expect(err).toBeTruthy();
+            done();
+        })
+    });
 })
