89164b1f7eec58900dc843c1db91056c98b02eae
Drobniac <drobniac@gmail.com>
2018-02-19 12:48:22 +0100

    Add important spec to parser-callback-test

--- test/csv-parser/parser-callback-test.js
+++ test/csv-parser/parser-callback-test.js
@@ -1,7 +1,12 @@
 let parse = require('lib/csv-parser').parse;
 
 describe('csv-parser.parse', function () {
+
+    it('puts the lotion on its skin', function () {
+        expect().nothing();
+    });
+
     it('should be a function', function () {
         expect(typeof parse).toMatch('function');
-    })
+    });
 })
