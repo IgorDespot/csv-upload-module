4c6dc5512f8b1cb65881c00e1ccb28be4e1f48f8
Vlada <vlada@bed.coma>
2018-02-21 17:14:22 +0100

    Fix message display for upload

--- services/upload/rest/post/index.js
+++ services/upload/rest/post/index.js
@@ -8,11 +8,11 @@ exports = module.exports = function (req, res, next) {
             });
         } else if (req.file == undefined) {
             res.render('upload', {
-                msg: 'Error: No file Selected!'
+                msg: 'Error: No file selected!'
             });
         } else {
             res.render('upload', {
-                msg: 'File Uploaded'
+                msg: 'File uploaded.'
             });
         }
     });
