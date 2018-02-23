afd7c113a76f5f17e34ab72a3229a1fa899c4726
Igor Despot <igordesspot@gmail.com>
2018-02-23 17:57:49 +0100

    Add test for orion-module

--- test/orion-module/index.js
+++ test/orion-module/index.js
@@ -1,9 +1,40 @@
 const frisby = require('frisby');
 let connection = require('lib/orion-connection');
 
+let config = require('../../config.json');
+
+const options = config["orion-options"];
+
+const path = config["orion-path"];
+
+
 describe('Orion module', function () {
 
     it('should be a function', function () {
         expect(typeof connection).toBe('function');
     });
+
+    it('should be a object', function () {
+        expect(typeof config).toBe('object');
+    });
+
+    it('should be a string', function() {
+        expect(typeof path).toBe('string');
+    });
+
+    it('should be defined', function() {
+        expect(path).toBeDefined();
+    });
+
+    it('should be a object', function () {
+        expect(typeof options).toEqual('object');
+    });
+
+    it('should contain Content-Type', function () {
+        expect(config).toEqual(jasmine.objectContaining({
+            "orion-path": jasmine.any(String)
+        }))
+    });
+
+    
 });
