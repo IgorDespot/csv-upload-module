5234140f4e0a716889464aba931c8b21de5463ef
Drobniac <drobniac@gmail.com>
2018-02-23 13:00:36 +0100

    Increase limit for rendering orion entities

--- routes/entity-list/index.js
+++ routes/entity-list/index.js
@@ -6,7 +6,9 @@ const NGSI = require('ngsijs');
 var connection = new NGSI.Connection("http://localhost:1026");
 
 router.get('/', function (req, res, next) {
-    connection.v2.listEntities()
+    connection.v2.listEntities({
+        limit: 200
+    })
     .then((response) => {
         res.send(response);
     });
