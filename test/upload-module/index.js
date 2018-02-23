6d77a34af70634bfbc6f96fef2b5a32aac5be39d
Igor Despot <igordesspot@gmail.com>
2018-02-23 18:53:01 +0100

    Add test for upload-module

--- test/upload-module/index.js
+++ test/upload-module/index.js
@@ -2,4 +2,13 @@ let upload = require('lib/upload-module');
 
 describe('Upload module', function () {
 
+    it('should be a function', function () {
+        expect(upload).toEqual(jasmine.any(Function));
+    });
+
+    it('should be a function', function () {
+        expect(upload.multer).toEqual(jasmine.any(Function));
+    });
+
+
 });
