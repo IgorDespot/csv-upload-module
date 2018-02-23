f398baf95580a7f67593dea8cc09122faf117953
Igor Despot <igordesspot@gmail.com>
2018-02-23 08:57:24 +0100

    Alter orion module

--- services/upload/rest/post/index.js
+++ services/upload/rest/post/index.js
@@ -4,6 +4,9 @@ upload = upload(upload.multer.memoryStorage());
 var csvParser = require('lib/csv-parser');
 var attrParser = require('lib/attribute-checker');
 
+const NGSI = require('ngsijs');
+var connection = new NGSI.Connection("http://localhost:1026");
+
 // Check differente errors and handle displaying them to user
 exports = module.exports = function (req, res, next) {
     upload(req, res, (err) => {
@@ -23,9 +26,22 @@ exports = module.exports = function (req, res, next) {
             csvParser.parsePromise(data, {delimiter: ';'})
             .then( (data) => {
                 return attrParser.promise(data);
-            }).then(
+            }).then( function (data) {
+                return Promise.all(data);
+            })
+            .then(
                 (data) => {
-                    console.log(data);
+                    return connection.v2.createEntity(
+                        data, {
+                            keyValues: true
+                        }
+                    );
+                }
+            ).then(
+                (res) => {
+                    console.log('it lives');
+                }, (err) => {
+                    console.log(err);
                 }
             );
         }
