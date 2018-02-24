216631b4dd039dbc2f7e340909645f6ea7a99281
Igor Despot <igordesspot@gmail.com>
2018-02-24 18:39:56 +0100

    Add tests for upload-module

--- test/upload-module/index.js
+++ test/upload-module/index.js
@@ -10,5 +10,24 @@ describe('Upload module', function () {
         expect(upload.multer).toEqual(jasmine.any(Function));
     });
 
+    it('should be a object', function () {
+        expect(upload.multer.memoryStorage()).toEqual(jasmine.any(Object));
+    });
+
+    it('should be a function', function () {
+        expect(upload.fileFilter).toEqual(jasmine.any(Function));
+    });
+
+    it('should be defined', function() {
+        expect(upload.fileFilter).toBeDefined();
+    });
+
+    it('should be a function', function () {
+        expect(upload.fileFilter).toEqual(jasmine.any(Function));
+    });
+
+    it('should recive 1 argument', function () {
+        expect(upload.fileFilter.length).not.toBeLessThan(1);
+    });
 
 });
