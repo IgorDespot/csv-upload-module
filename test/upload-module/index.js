48cffab739188fd4571cc23d2f201c7c39b94202
Igor Despot <igordesspot@gmail.com>
2018-02-25 18:10:07 +0100

    Add test for upload-module

--- test/upload-module/index.js
+++ test/upload-module/index.js
@@ -1,4 +1,9 @@
 let upload = require('lib/upload-module');
+let config = require('../../config.json');
+
+const fileExtentions = config['file-extentions'];
+
+console.log(fileExtentions);
 
 describe('Upload module', function () {
 
@@ -18,7 +23,7 @@ describe('Upload module', function () {
         expect(upload.fileFilter).toEqual(jasmine.any(Function));
     });
 
-    it('should be defined', function() {
+    it('should be defined', function () {
         expect(upload.fileFilter).toBeDefined();
     });
 
@@ -30,4 +35,21 @@ describe('Upload module', function () {
         expect(upload.fileFilter.length).not.toBeLessThan(1);
     });
 
+    it('should be array', function () {
+        expect(fileExtentions).toEqual(jasmine.any(Array));
+    });
+
+    it('should be defined', function () {
+        expect(fileExtentions).toBeDefined();
+    });
+
+    it('should be defined', function () {
+        expect(config).toBeDefined();
+    });
+
+    it('should contain Content-Type', function () {
+        expect(config).toEqual(jasmine.objectContaining({
+            "file-extentions": jasmine.any(Array)
+        }));
+    });
 });
