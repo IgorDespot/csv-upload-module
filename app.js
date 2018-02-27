d4e0c6bab07977c1410a3a6fa631358e151c0958
Vlada <vlada@bed.coma>
2018-02-27 12:40:59 +0100

    Write logs to a file

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 const expbs = require('express-handlebars');
+const fs = require('fs');
 
 var app = express();
 // Require login module
@@ -27,7 +28,14 @@ app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+//log requests to a file
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
+app.use(logger('common', {stream: accessLogStream}));
+
+//log requests to a console
 app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -57,5 +65,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('login/error');
 });
+
+
 
 module.exports = app;
