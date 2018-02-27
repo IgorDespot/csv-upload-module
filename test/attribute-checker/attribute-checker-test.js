9b54d9132069bbc86eb8200b28d2e95769815cdc
Drobniac <drobniac@gmail.com>
2018-02-27 09:13:56 +0100

    Add more tests for attribute checker

--- test/attribute-checker/attribute-checker-test.js
+++ test/attribute-checker/attribute-checker-test.js
@@ -22,7 +22,25 @@ describe('attribute-checker', function () {
                 delimiter: ';'
             }
         );
-    })
+    });
+
+    it(`
+        shouldn't throw an error on correct csv input
+        with preset attribute ruleset (in this case DepositPoint)
+    `, function (done) {
+        fileParser.parse(
+            './test/attribute-checker/test-3.csv',
+            function (err, data) {
+                attrChecker(data, function (err, data) {
+                    console.log(data);
+                    expect(err).toBeFalsy();
+                    done();
+                });
+            }, {
+                delimiter: ';'
+            }
+        );
+    });
 
     it(`
         should provide an error when the ruleset
