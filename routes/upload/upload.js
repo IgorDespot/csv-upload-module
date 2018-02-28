67b5fb4e655981f1fadceadfebe8d04bac7cc86a
Marija Borisov <marija.borisov@gmail.com>
2018-02-28 15:07:00 +0100

    Change authentication a little bit and add some small changes

--- routes/upload/upload.js
+++ routes/upload/upload.js
@@ -3,6 +3,7 @@ let router = express.Router();
 let {get, post} = require('services/upload/rest');
 
 router.get('/', function (req, res, next) {
+    console.log("upload\n");
     get(req, res, next);
 });
 
