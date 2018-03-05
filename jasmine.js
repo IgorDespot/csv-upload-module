24777a27bc75f5f0b0a363b94d3eacec82ecae1e
Drobniac <drobniac@gmail.com>
2018-03-05 13:50:50 +0100

    Pass server control to jasmine.js

--- jasmine.js
+++ jasmine.js
@@ -1,10 +1,12 @@
 require('app-module-path').addPath( __dirname );
+var server = require('bin/www');
 var Jasmine = require('jasmine');
 var jasmine = new Jasmine();
 
 jasmine.loadConfigFile('spec/support/jasmine.json');
 
 jasmine.onComplete(function(passed) {
+    server.close();
     if(passed) {
         console.log('All specs have passed');
     }
