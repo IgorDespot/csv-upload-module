f192e588b3cd3b7650dde21dd742b86374a82fdc
Igor Despot <igordesspot@gmail.com>
2018-03-05 16:23:52 +0100

    Fix import for orion-module

--- services/entities/rest/get/index.js
+++ services/entities/rest/get/index.js
@@ -1,9 +1,11 @@
-const getAll = require('lib/orion-module-new');
+const getAll = require('lib/orion-module-new').listEntities;
 
 exports = module.exports = function(req, res, next) {
+
   getAll().then(function(entities) {
     res.render('entities', {data: entities});
   }).catch((error) => {
     res.render('entities', {msg: error});
   });
+
 }
