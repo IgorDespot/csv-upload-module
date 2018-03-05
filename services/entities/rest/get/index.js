87a13d6c6ae534a5ac6cee853eaf00bc3db278aa
Igor Despot <igordesspot@gmail.com>
2018-03-05 10:55:42 +0100

    Add new orion module to api

--- services/entities/rest/get/index.js
+++ services/entities/rest/get/index.js
@@ -1,7 +1,8 @@
 const all = require('lib/orion-module').listAll;
+const getAll = require('lib/orion-module-new');
 
 exports = module.exports = function(req, res, next) {
-  all().then(function(entities) {
+  getAll().then(function(entities) {
     res.send(entities)
   }).catch((error) => {
     res.send(error)
