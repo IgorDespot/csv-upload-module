8767c87951c35b4eeb68b9d60807dbf49df9edf1
Igor Despot <igordesspot@gmail.com>
2018-03-06 12:30:10 +0100

    Add con.log information about enities

--- services/entities/rest/get/index.js
+++ services/entities/rest/get/index.js
@@ -1,8 +1,12 @@
 const getAll = require('lib/orion-module-new').listEntities;
+const path = require('lib/orion-module-new').orionPath;
 
 exports = module.exports = function(req, res, next) {
 
   getAll().then(function(entities) {
+    entities.forEach(element => {
+      console.log("{\n Connecting to: "+ path + "\n\t" + JSON.stringify(element, null, "\t") + "Successfuly retrieved entity \n}" + "\n");
+    });
     res.render('entities', {data: entities});
   }).catch((error) => {
     res.render('entities', {msg: error});
