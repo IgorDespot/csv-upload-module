039e68bac1f7f034b71ffb87db13d37b2bb0a5e9
Igor Despot <igordesspot@gmail.com>
2018-03-08 18:04:10 +0100

    Add update to ngsi connector

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let router = express.Router();
-let {get, post} = require('services/api/rest');
+let {get, post, patch} = require('services/api/rest');
 
 router.get('/', function (req, res, next) {
     get(req, res, next);
@@ -8,6 +8,10 @@ router.get('/', function (req, res, next) {
 
 router.post('/', (req, res) => {
     post(req, res);
+});
+
+router.patch('/update', (req, res) => {
+    patch(req, res);
 });
 
 module.exports = router;
