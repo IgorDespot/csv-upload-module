0bfe8c1d2318c0c86afbd19da90d751e757e5a55
Igor Despot <igordesspot@gmail.com>
2018-03-12 16:06:17 +0100

    Add create entity options to module

--- routes/api/index.js
+++ routes/api/index.js
@@ -22,11 +22,11 @@ router.get('/types/:id', function (req, res, next) {
     get.type(req, res, next);
 });
 
-router.post('/', (req, res) => {
-    post(req, res);
+router.post('/entities', function (req, res, next) {
+    post.createEntity(req,res, next);
 });
 
-router.post('/update', (req, res) => {
+router.post('/entities/update', (req, res) => {
     patch(req, res);
 });
 
