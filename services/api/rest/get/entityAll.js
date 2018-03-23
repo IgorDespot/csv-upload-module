3d2421d1b89675cc6cd7890411d2fba02f9ce007
Igor Despot <igordesspot@gmail.com>
2018-03-23 17:15:43 +0100

    Remove bad code from entitiy.all service

--- services/api/rest/get/entityAll.js
+++ services/api/rest/get/entityAll.js
@@ -4,9 +4,8 @@ exports = module.exports = function (req, res, next) {
 
   let service = req.headers['fiware-service'];
   let service_path = req.headers['fiware-servicepath'];
-  let type_id = req.params.id;
 
-  entity.listEntitiesPromise(service, service_path, type_id).then(function (entities) {
+  entity.listEntitiesPromise(service, service_path).then(function (entities) {
     res.json(entities)
   }).catch((error) => {
     res.json(error)
