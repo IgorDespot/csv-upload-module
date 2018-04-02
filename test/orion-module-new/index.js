eda7ede64f89958e22e401f20c46540c20ea18ae
Igor <igor.despot@eng.rs>
2018-04-02 16:36:08 +0200

    Add new response function for orion module responses. Add test for it.

--- test/orion-module-new/index.js
+++ test/orion-module-new/index.js
@@ -1,46 +1,26 @@
-const config = require('config');
+const {
+  responseSuccess,
+  responseFail
+} = require('lib/orion-module-new/entity/response');
 
-const orionPath = config["orion-path"];
+const entity = {
+    "id": "8EB19",
+    "type": "DepositPoint",
+}
 
-const entities = require('lib/orion-module-new').listEntities;
-const create = require('lib/orion-module-new').createEntity;
+describe('function checks', () => {
 
-xdescribe('toBeDefined checks', () => {
-
-  it('config should be defined or error will throw', () => {
-    expect(config).toBeDefined();
+  it('should return json object when entity is passed', () => {
+    expect(responseSuccess('CREATE'))
+    .toEqual(jasmine.any(Object));
+    expect(responseFail(entity,'UPDATE', 'GRESKA'))
+    console.log(responseSuccess(entity,'CREATE'))
   });
 
-  it('path should be defined or error will throw', () => {
-    expect(orionPath).toBeDefined();
+  it('should return json object when entity is passed', () => {
+    expect(responseFail(entity,'UPDATE', 'GRESKA'))
+    .toEqual(jasmine.any(Object));
   });
 
-});
 
-xdescribe('function checks', () => {
-
-  it('should be function',  () => {
-      expect(entities).toEqual(jasmine.any(Function));
-  });
-
-  it(`
-    should make call to orion contex broker using api
-    and return array of objects representing entities
-  `, () => {
-    entities()
-    .then( (data) => {
-      expect(data).toEqual(jasmine.any(Object));
-      done();
-    })
-    .catch( () => {
-      done.fail('Should not come here');
-    });
-  });
-
-  it(`
-    should throw error if passed entity is undefined
-  `, function () {
-    expect(function(){create()}).toThrowError('Entity must be defined');
-  });  
-  
 });
