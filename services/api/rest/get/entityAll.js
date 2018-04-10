827ef1f3e6a109c3c605b82e247e22afb37086e5
Igor <igor.despot@eng.rs>
2018-04-10 12:06:51 +0200

    Changed http library for get all entities from request to request prom.

--- services/api/rest/get/entityAll.js
+++ services/api/rest/get/entityAll.js
@@ -1,13 +1,29 @@
 const { entity }    = require('lib/orion-module-new');
+const rp            = require('request-promise');
+const config        = require('../../../../config.json');
+const orionPath     = config['orion-path']
 
 exports = module.exports = function (req, res, next) {
 
   let service = req.headers['fiware-service'];
   let service_path = req.headers['fiware-servicepath'];
 
-  entity.listEntitiesPromise(service, service_path).then(function (entities) {
-    res.json(entities)
-  }).catch((error) => {
-    res.json(error)
-  });
+  let options = {
+    method: "GET",
+    uri: orionPath + 'entities/',
+    headers: {
+      "Fiware-Service": service,
+      "Fiware-ServicePath": service_path
+    },
+    json: true
+  }
+
+  rp(options)
+    .then((entities) => {
+      res.json(entities)
+    })
+    .catch((error) => {
+      res.json(error)
+    });
+
 }
