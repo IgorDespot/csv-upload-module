0a607037f14f36c8a84ab9708d252aa6f77a10fb
Igor <igor.despot@eng.rs>
2018-04-10 12:21:21 +0200

    Chaged code to support new http library, add response, refactor code.

--- services/api/rest/post/createEntity.js
+++ services/api/rest/post/createEntity.js
@@ -1,59 +1,103 @@
-let uploadModule = require('lib/upload-module');
-let upload = uploadModule(uploadModule.multer.memoryStorage());
-let payload = require('./payload');
-var rp = require('request-promise');
-var config = require('../../../../config.json');
-var orionPath = config['orion-path'];
+const uploadModule    = require('lib/upload-module');
+const upload          = uploadModule(uploadModule.multer.memoryStorage());
+const sucessR         = require('../payload').responseSuccess;
+const failedR         = require('../payload').responseFail;
+const rp              = require('request-promise');
+const config          = require('../../../../config.json');
+const orionPath       = config['orion-path'];
+const ngsiConverter   = require('lib/ngsi-converter');
+const { entity }      = require('lib/orion-module-new');
 
-var ngsiConverter = require('lib/ngsi-converter');
-const {
-    entity
-} = require('lib/orion-module-new');
+console.log(sucessR);
 
-// Check different errors and handle displaying them to user
 exports = module.exports = function (req, res, next) {
 
-    let service = req.headers['fiware-service'];
-    let service_path = req.headers['fiware-servicepath'];
+  let service = req.headers['fiware-service'];
+  let service_path = req.headers['fiware-servicepath'];
 
-    upload(req, res, (err) => {
-        if (err) {
-            res.json(err)
-        } else if (req.files == undefined) {
-            res.json('U try to send empty file fail')
-        } else {
-            var data = req.files[0].buffer.toString();
-            var extension = uploadModule.getFileExtension(req.files[0]);
-            var empty = [];
-            ngsiConverter(data, extension)
-                .then((data) => {
-                    var empty = [];
-                   for (let i = 0; i < data.length; i++) {
-                    var options = {
-                        method: 'POST',
-                        uri: orionPath + 'entities?options=keyValues',
-                        headers: {
-                            "Content-Type": 'application/json',
-                            "Fiware-Service": service,
-                            "Fiware-ServicePath": service_path
-                        },
-                        body: data[i],
-                        json: true // Automatically stringifies the body to JSON
-                    };
-                    empty.push(rp(options)
-                    .then((res)=> {return Promise.resolve('Yes')})
-                    .catch((err)=>{return Promise.resolve(err)}))
-                   }
-                    Promise.all(empty).then((results) => {
-                        res.json(results)
-                    }).catch((error) => {
-                        console.log(error)
-                    });
-                }).catch((err) => {
-                    res.json(err);
-                })
-        }
-    });
+  upload(req, res, (err) => {
+    if (err) {
+      res.json(err)
+    } else if (req.files == undefined) {
+      res.json('Please select file.')
+    } else {
+      var data = req.files[0].buffer.toString();
+      var extension = uploadModule.getFileExtension(req.files[0]);
+      var empty = [];
+
+      ngsiConverter(data,extension)
+        .then((data) => {
+          var empty = [];
+          for (let i = 0; i < data.length; i++) {
+            var options = {
+              method: 'POST',
+              uri: orionPath + 'entities?options=keyValues',
+              headers: {
+                  "Content-Type": 'application/json',
+                  "Fiware-Service": service,
+                  "Fiware-ServicePath": service_path
+              },
+              body: data[i],
+              json: true
+            }
+            empty.push(rp(options)
+             .then((res) => {
+               return Promise.resolve(sucessR(data[i], 'CREATE'))
+             })
+             .catch((err) => {
+               return Promise.resolve(failedR(data[i], 'CREATE', err.error))
+             }))
+          }
+          Promise.all(empty)
+            .then((results) => {
+              res.json([{
+                "Successfuly created:": numOfSuccess(results),
+                "Number of errors happend while creating:": numOfFails(results)
+              }, results])
+            }).catch((error) => {
+              res.json(error)
+            })
+        }).catch((error) => {
+          var data = error.result;
+          var errors = error.err.map((err) => {
+              return err.message;
+          });
+          for (let i = 0; i < data.length; i++) {
+            var options = {
+              method: 'POST',
+              uri: orionPath + 'entities?options=keyValues',
+              headers: {
+                  "Content-Type": 'application/json',
+                  "Fiware-Service": service,
+                  "Fiware-ServicePath": service_path
+              },
+              body: data[i],
+              json: true
+            };
+            empty.push(rp(options)
+             .then((res) => {
+               return Promise.resolve(sucessR(data[i], 'CREATE'))
+             })
+             .catch((err) => {
+               return Promise.resolve(failedR(data[i], 'CREATE', err.error))
+             }))
+          }
+          Promise.all(empty)
+            .then((results) => {
+              res.json([{
+                "Attribute checke errors:": sizeObj(errors),
+                "Successfuly created:": numOfSuccess(results),
+                "Number of erros happend while creating:": numOfFails(results)
+              },
+              errors,
+              results
+            ])
+            }).catch((error) => {
+              res.json(error)
+            });
+        });
+    }
+  });
 }
 
 function entityFailWrapper(promise) {
