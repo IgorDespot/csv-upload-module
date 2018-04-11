d6eac9908455f5b8819dfdc1c5c6c7883b2be440
Igor <igor.despot@eng.rs>
2018-04-11 09:21:15 +0200

    Updated getOneEntity to match new code logic and add better res code.

--- services/api/rest/get/entityOne.js
+++ services/api/rest/get/entityOne.js
@@ -1,4 +1,7 @@
 const { entity }    = require('lib/orion-module-new');
+const rp            = require('request-promise');
+const config        = require('../../../../config.json');
+const orionPath     = config['orion-path']
 
 exports = module.exports = function (req, res, next) {
 
@@ -6,9 +9,28 @@ exports = module.exports = function (req, res, next) {
   let service_path = req.headers['fiware-servicepath'];
   let entity_id = req.params.id;
 
-  entity.singleEntityPromise(service, service_path, entity_id).then(function (entity) {
-    res.json(entity)
-  }).catch((error) => {
-    res.json(error)
-  });
+  let options = {
+    method: "GET",
+    uri: orionPath + 'entities/' + entity_id,
+    headers: {
+      "Fiware-Service": service,
+      "Fiware-ServicePath": service_path
+    },
+    json: true
+  }
+
+  rp(options)
+    .then((entity) => {
+      res.json(entity)
+    })
+    .catch((error) => {
+     if(error.statusCode === 404) {
+       res.status(404).json('The resource (entity, subscription, etc.) referred in the request has not been found')
+     } else if (error.error['code'] == 'ECONNREFUSED') {
+      res.status(503).json('There are no server resources to fulfill the client request.')
+     } else {
+       res.json(error)
+     }
+
+    });
 }
