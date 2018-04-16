208750943019f42bd350be3e6b28339a5e97cd3c
Igor <igor.despot@eng.rs>
2018-04-16 10:42:27 +0200

    Refactor code, add summary and test for it.

--- test/responsePayload/index.js
+++ test/responsePayload/index.js
@@ -3,10 +3,17 @@ const {
   responseFail
 } = require('services/api/rest/payload');
 
+const { numOfErrors,numOfSucess,numOfFails } = require('services/api/rest/summary');
+const resposne = require('./response.json');
+const failResponse = require('./responseFail.json');
+
 const entity = {
     "id": "8EB19",
     "type": "DepositPoint",
 }
+
+const error = ["Error1", "Error2", "Error3", "Error4"];
+const noErr = [];
 
 describe('function checks', () => {
 
@@ -21,5 +28,20 @@ describe('function checks', () => {
     .toEqual(jasmine.any(Object));
   });
 
+  it('should return number of errors when we passed errror object', () => {
+    expect(numOfErrors(error)).toEqual(jasmine.any(Number));
+  });
+
+  it('should return 0 when there is no errros', () => {
+    expect(numOfErrors(noErr)).toEqual(0);
+  });
+
+  it('should return number of successfuly created entites if there are any', () => {
+    expect(numOfSucess(resposne)).toEqual(jasmine.any(Number));
+  });
+
+  it('should return number of fails while creating entities if there are any', () => {
+    expect(numOfFails(failResponse)).toEqual(jasmine.any(Number));
+  });
 
 });
