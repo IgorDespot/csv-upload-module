a841dc86922681c80c2ee99d9ab1bcc145782aca
Igor <igor.despot@eng.rs>
2018-04-16 12:21:29 +0200

    Refactor code and add setOptions function and tests to project.

--- test/responsePayload/index.js
+++ test/responsePayload/index.js
@@ -3,17 +3,27 @@ const {
   responseFail
 } = require('services/api/rest/payload');
 
-const { numOfErrors,numOfSucess,numOfFails } = require('services/api/rest/summary');
-const { setOptionsPost,setOptionsPatch } = require('services/api/rest/setOPtions');
+const {
+  numOfErrors,
+  numOfSucess,
+  numOfFails
+} = require('services/api/rest/summary');
+const {
+  setOptionsPost,
+  setOptionsPatch,
+  setOPtionsGetSingle,
+  setOPtionsGetAll,
+  setOtionsGetByType
+} = require('services/api/rest/setOPtions');
 const resposne = require('./response.json');
 const failResponse = require('./responseFail.json');
-const config  = require('config.json');
+const config = require('config.json');
 const orionPath = config['orion-path']
 
 const entity = {
-    "id": "8EB19",
-    "type": "DepositPoint",
-    "description": "Container"
+  "id": "8EB19",
+  "type": "DepositPoint",
+  "description": "Container"
 }
 
 const error = ["Error1", "Error2", "Error3", "Error4"];
@@ -23,13 +33,13 @@ describe('function checks', () => {
 
   it('should return json object when entity is passed', () => {
     expect(responseSuccess('CREATE'))
-    .toEqual(jasmine.any(Object));
-    expect(responseFail(entity,'UPDATE', 'GRESKA'))
+      .toEqual(jasmine.any(Object));
+    expect(responseFail(entity, 'UPDATE', 'GRESKA'))
   });
 
   it('should return json object when entity is passed', () => {
-    expect(responseFail(entity,'UPDATE', 'GRESKA'))
-    .toEqual(jasmine.any(Object));
+    expect(responseFail(entity, 'UPDATE', 'GRESKA'))
+      .toEqual(jasmine.any(Object));
   });
 
   it('should return number of errors when we passed errror object', () => {
@@ -49,14 +59,27 @@ describe('function checks', () => {
   });
 
   it('should return options object whit provided arguments', () => {
-    expect(setOptionsPost('waste4think', '/deusto/w4t/zamudio/test', entity,orionPath))
-    .toEqual(jasmine.any(Object));
+    expect(setOptionsPost('waste4think', '/deusto/w4t/zamudio/test', entity, orionPath))
+      .toEqual(jasmine.any(Object));
   });
 
   it('should return object for patch calls whith provided arguments', () => {
-    expect(setOptionsPatch('waste4think', '/deusto/w4t/zamudio/test', entity,orionPath))
-    .toEqual(jasmine.any(Object));
-    console.log(setOptionsPatch('waste4think', '/deusto/w4t/zamudio/test', entity,orionPath))
+    expect(setOptionsPatch('waste4think', '/deusto/w4t/zamudio/test', entity, orionPath))
+      .toEqual(jasmine.any(Object));
   });
 
+  it('should return options object for getting single entity form orioncb', () => {
+    expect(setOPtionsGetSingle('waste4think', '/deusto/w4t/zamudio/test', entity.id, orionPath))
+      .toEqual(jasmine.any(Object));
+  });
+
+  it('should return options object for getting all entities from ocb',() => {
+    expect(setOPtionsGetAll('waste4think', '/deusto/w4t/zamudio/test',orionPath))
+    .toEqual(jasmine.any(Object));
+  });
+
+  it('should return options object for getting all entities by type from ocb',() => {
+    expect(setOtionsGetByType('waste4think', '/deusto/w4t/zamudio/test',orionPath, entity.type))
+    .toEqual(jasmine.any(Object));
+  });
 });
