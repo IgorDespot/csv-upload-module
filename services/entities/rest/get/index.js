2df7a89f9b36521c78fbff09694ea005b45b35b4
Igor <igor.despot@eng.rs>
2018-04-17 16:06:54 +0200

    Code refactor

--- services/entities/rest/get/index.js
+++ services/entities/rest/get/index.js
@@ -4,7 +4,6 @@ const path = require('lib/orion-module-new').orionPath;
 exports = module.exports = function(req, res, next) {
 
   getAll().then(function(entities) {
-    console.log(entities)
     res.render('entities', {data: entities});
   }).catch((error) => {
     res.render('entities', {msg: error});
