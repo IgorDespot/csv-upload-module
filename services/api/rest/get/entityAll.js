bfc744105f2de245cffbf72662dee1dc84495549
Igor <igor.despot@eng.rs>
2018-05-22 09:49:20 +0200

    Refactor code for get all entities service.

--- services/api/rest/get/entityAll.js
+++ services/api/rest/get/entityAll.js
@@ -1,32 +1,29 @@
-const { entity }    = require('lib/orion-module-new');
-const rp            = require('request-promise');
-const config        = require('../../../../config.json');
-const orionPath     = config['orion-path']
+const rp = require('request-promise');
+const config = require('../../../../config.json');
 
-exports = module.exports = function (req, res, next) {
+const orionPath = config['orion-path'];
 
-  let service = req.headers['fiware-service'];
-  let service_path = req.headers['fiware-servicepath'];
+module.exports = (req, res, next) => {
+  const service = req.headers['fiware-service'];
+  const servicePath = req.headers['fiware-servicepath'];
 
-  let options = {
-    method: "GET",
-    uri: orionPath + 'entities/',
+  const options = {
+    method: 'GET',
+    uri: `${orionPath}entities/`,
     headers: {
-      "Fiware-Service": service,
-      "Fiware-ServicePath": service_path
+      'Fiware-Service': service,
+      'Fiware-ServicePath': servicePath
     },
     json: true
-  }
+  };
 
   rp(options)
     .then((entities) => {
-      res.json(entities)
+      res.json(entities);
     })
     .catch((error) => {
-      if (error.error['code'] == 'ECONNREFUSED')
-        res.status(503).json('There are no server resources to fulfill the client request.')
-      else
-        res.json(error)
+      if (error.error.code === 'ECONNREFUSED') {
+        res.status(503).json('There are no server resources to fulfill the client request.');
+      } else { res.json(error); }
     });
-
-}
+};
