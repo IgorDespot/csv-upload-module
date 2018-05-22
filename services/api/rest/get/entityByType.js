b54d386089a873225dae8f7635457766f6ffd847
Igor <igor.despot@eng.rs>
2018-05-22 10:09:06 +0200

    Refactor get entity by type.

--- services/api/rest/get/entityByType.js
+++ services/api/rest/get/entityByType.js
@@ -1,41 +1,37 @@
-const {
-  entity
-} = require('lib/orion-module-new');
 const rp = require('request-promise');
 const config = require('../../../../config.json');
-const orionPath = config['orion-path']
 
-exports = module.exports = function (req, res, next) {
+const orionPath = config['orion-path'];
 
-  let service = req.headers['fiware-service'];
-  let service_path = req.headers['fiware-servicepath'];
-  let type = req.params.id;
+module.exports = (req, res, next) => {
+  const service = req.headers['fiware-service'];
+  const servicePath = req.headers['fiware-servicepath'];
+  const type = req.params.id;
 
-  let options = {
-    method: "GET",
-    uri: orionPath + 'entities/?type=' + type,
+  const options = {
+    method: 'GET',
+    uri: `${orionPath}entities/?type=${type}`,
     headers: {
-      "Fiware-Service": service,
-      "Fiware-ServicePath": service_path
+      'Fiware-Service': service,
+      'Fiware-ServicePath': servicePath
     },
     json: true
-  }
+  };
 
   rp(options)
     .then((type) => {
-      if (type.length === 0)
-        res.status(404).json('The resource (entity, subscription,type etc.) referred in the request has not been found')
-      else
-        res.json(type)
+      if (type.length === 0) {
+        res.status(404)
+          .json('The resource (entity, subscription,type etc.) referred in the request has not been found');
+      } else { res.json(type); }
     })
     .catch((error) => {
       if (error.statusCode === 404) {
-        res.status(404).json('The resource (entity, subscription, etc.) referred in the request has not been found')
-      } else if (error.error['code'] == 'ECONNREFUSED') {
-        res.status(503).json('There are no server resources to fulfill the client request.')
+        res.status(404).json('The resource (entity, subscription, etc.) referred in the request has not been found');
+      } else if (error.error.code === 'ECONNREFUSED') {
+        res.status(503).json('There are no server resources to fulfill the client request.');
       } else {
-        res.json(error)
+        res.json(error);
       }
-
     });
-}
+};
