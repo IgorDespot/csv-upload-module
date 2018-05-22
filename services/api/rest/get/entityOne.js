1a651c6ab7e25c69c41b6b785a68b4209fa7386a
Igor <igor.despot@eng.rs>
2018-05-22 10:15:52 +0200

    Refactor code for get single entity.

--- services/api/rest/get/entityOne.js
+++ services/api/rest/get/entityOne.js
@@ -1,36 +1,36 @@
-const { entity }    = require('lib/orion-module-new');
-const rp            = require('request-promise');
-const config        = require('../../../../config.json');
-const orionPath     = config['orion-path']
+const rp = require('request-promise');
+const config = require('../../../../config.json');
 
-exports = module.exports = function (req, res, next) {
+const orionPath = config['orion-path'];
 
-  let service = req.headers['fiware-service'];
-  let service_path = req.headers['fiware-servicepath'];
-  let entity_id = req.params.id;
+module.exports = (req, res, next) => {
+  const service = req.headers['fiware-service'];
+  const servicePath = req.headers['fiware-servicepath'];
+  const entityId = req.params.id;
 
-  let options = {
-    method: "GET",
-    uri: orionPath + 'entities/' + entity_id,
+  const options = {
+    method: 'GET',
+    uri: `${orionPath}entities/${entityId}`,
     headers: {
-      "Fiware-Service": service,
-      "Fiware-ServicePath": service_path
+      'Fiware-Service': service,
+      'Fiware-ServicePath': servicePath
     },
     json: true
-  }
+  };
 
   rp(options)
     .then((entity) => {
-      res.json(entity)
+      res.json(entity);
     })
     .catch((error) => {
-     if(error.statusCode === 404) {
-       res.status(404).json('The resource (entity, subscription, etc.) referred in the request has not been found')
-     } else if (error.error['code'] == 'ECONNREFUSED') {
-      res.status(503).json('There are no server resources to fulfill the client request.')
-     } else {
-       res.json(error)
-     }
-
+      if (error.statusCode === 404) {
+        res.status(404)
+          .json('The resource (entity, subscription, etc.) referred in the request has not been found');
+      } else if (error.error.code === 'ECONNREFUSED') {
+        res.status(503)
+          .json('There are no server resources to fulfill the client request.');
+      } else {
+        res.json(error);
+      }
     });
-}
+};
