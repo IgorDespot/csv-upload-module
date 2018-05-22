6a8c1e8ed31258e79a9cf4fbf0c7857029353052
Igor <igor.despot@eng.rs>
2018-05-22 09:40:38 +0200

    Refactor routes.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,33 +1,34 @@
 const express = require('express');
+
 const router = express.Router();
 const { get, post, patch } = require('services/api/rest');
 
-router.get('/entities', function (req, res, next) {
-    get.entities(req, res, next);
+router.get('/entities', (req, res, next) => {
+  get.entities(req, res, next);
 });
 
-router.get('/entities/:id', function (req, res, next) {
-    get.entity(req, res, next);
+router.get('/entities/:id', (req, res, next) => {
+  get.entity(req, res, next);
 });
 
-router.get('/entities/type/:id', function (req, res, next) {
-    get.entityType(req, res, next);
+router.get('/entities/type/:id', (req, res, next) => {
+  get.entityType(req, res, next);
 });
 
-router.get('/types', function (req, res, next) {
-    get.types(req, res, next);
+router.get('/types', (req, res, next) => {
+  get.types(req, res, next);
 });
 
-router.get('/types/:id', function (req, res, next) {
-    get.type(req, res, next);
+router.get('/types/:id', (req, res, next) => {
+  get.type(req, res, next);
 });
 
-router.post('/entities', function (req, res, next) {
-    post.createEntity(req,res, next);
+router.post('/entities', (req, res, next) => {
+  post.createEntity(req, res, next);
 });
 
-router.post('/entities/update', function (req, res, next) {
-    patch.updateEntity(req, res, next);
+router.post('/entities/update', (req, res, next) => {
+  patch.updateEntity(req, res, next);
 });
 
 module.exports = router;
